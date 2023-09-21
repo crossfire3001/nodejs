2d32d33eb73db0b8913e5d94cbd8740859c26f48
crossfire3001 <obiwan2035@mail.ru>
2023-09-21 21:37:57 +0500

    css min

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,14 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
 
 function defaultTask() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./dist'));
 }
 
