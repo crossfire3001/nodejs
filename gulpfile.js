7ee298d1099a7dfb7a71ea4c472cee8edd009f3b
crossfire3001 <obiwan2035@mail.ru>
2023-09-21 21:12:56 +0500

    sass with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    console.log('hi');
-    cb();
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function defaultTask() {
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist'));
 }
 
 exports.default = defaultTask
