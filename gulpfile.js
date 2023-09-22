29a349609342e0cf7ad5d3422b4299c52a127172
crossfire3001 <obiwan2035@mail.ru>
2023-09-22 20:40:11 +0500

    merging files

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,13 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
+const concatCss = require('gulp-concat-css');
+
 
 function defaultTask() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(concatCss("style.css"))
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./dist'));
